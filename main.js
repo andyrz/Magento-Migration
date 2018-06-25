ee59e8384f6bf233c079c7550d3591feacea9132
Andy Zaluski <zaluskiandy@gmail.com>
2018-06-25 10:23:23 -0400

    clean up

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-alert("JS open successfully");
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
